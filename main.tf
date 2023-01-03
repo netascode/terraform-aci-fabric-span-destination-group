2aad951d4402205cca9d8c9128ad5eed41abb532
Justyna Chowaniec (juchowan) <juchowan@cisco.com>
2023-01-03 18:26:21 +0100

    first commit

--- main.tf
+++ main.tf
@@ -1,9 +1,33 @@
-resource "aci_rest_managed" "fvTenant" {
-  dn         = "uni/tn-${var.name}"
-  class_name = "fvTenant"
+resource "aci_rest_managed" "spanDestGrp" {
+  dn         = "uni/fabric/destgrp-${var.name}"
+  class_name = "spanDestGrp"
   content = {
-    name      = var.name
-    nameAlias = var.alias
-    descr     = var.description
+    name  = var.name
+    descr = var.description
+  }
+}
+
+resource "aci_rest_managed" "spanDest" {
+  dn         = "${aci_rest_managed.spanDestGrp.id}/dest-${var.name}"
+  class_name = "spanDest"
+  content = {
+    name  = var.name
+    descr = ""
+  }
+}
+
+resource "aci_rest_managed" "spanRsDestEpg" {
+  dn         = "${aci_rest_managed.spanDest.id}/rsdestEpg"
+  class_name = "spanRsDestEpg"
+  content = {
+    ip          = var.ip
+    srcIpPrefix = var.source_prefix
+    dscp        = var.dscp
+    flowId      = var.flow_id
+    mtu         = var.mtu
+    ttl         = var.ttl
+    ver         = "ver${var.span_version}"
+    verEnforced = var.enforce_version == true ? "yes" : "no"
+    tDn         = "uni/tn-${var.tenant}/ap-${var.application_profile}/epg-${var.endpoint_group}"
   }
 }
